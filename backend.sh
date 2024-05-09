b878c53ea6185561943d64633d87d7408f4d1773
Sree201 <sreesirinath4@gmail.com>
2024-05-08 21:00:30 -0700

    backend

--- backend.sh
+++ backend.sh
@@ -82,7 +82,7 @@ VALIDATE $? "Start httpd"
 systemctl enable httpd &>>$LOGFILE
 VALIDATE $? "Starting httpd"
 
-mysql -h localhost -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
+mysql -h NT Service\MSSQL$SQLEXPRESS -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
 VALIDATE $? "Schema loading"
 
 systemctl restart backend &>>$LOGFILE

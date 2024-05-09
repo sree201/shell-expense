b3e4f70ffaae24ac6ba8309790711011d08fcedd
Sree201 <sreesirinath4@gmail.com>
2024-05-09 14:58:04 -0700

    backend

--- backend.sh
+++ backend.sh
@@ -82,7 +82,7 @@ VALIDATE $? "Start httpd"
 systemctl enable httpd &>>$LOGFILE
 VALIDATE $? "Starting httpd"
 
-mysql -h MSSQL$SQLEXPRESS -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
+mysql -h db.daws78s.online -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
 VALIDATE $? "Schema loading"
 
 systemctl restart backend &>>$LOGFILE

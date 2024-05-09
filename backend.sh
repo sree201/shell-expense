e367a6d7a82c2d93b7022f9c098ccb594e22f43d
Sree201 <sreesirinath4@gmail.com>
2024-05-09 16:36:08 -0700

    backend

--- backend.sh
+++ backend.sh
@@ -82,7 +82,7 @@ VALIDATE $? "Start httpd"
 systemctl enable httpd &>>$LOGFILE
 VALIDATE $? "Starting httpd"
 
-mysql -h 192.168.1.66 -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
+mysql -h 192.168.1.254 -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
 VALIDATE $? "Schema loading"
 
 systemctl restart backend &>>$LOGFILE

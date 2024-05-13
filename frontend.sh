25b42acc8323c88abb5d197ce80e7c087d233ab3
Sree201 <sreesirinath4@gmail.com>
2024-05-12 21:00:06 -0700

    frontend

--- frontend.sh
+++ frontend.sh
@@ -1,66 +1,34 @@
-# #!/bin/bash
-
-# USERID=$(id -u)
-# TIMESTAMP=$(date +%F-%H-%M-%S)
-# SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-# LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-# R="\e[31m"
-# G="\e[32m"
-# Y="\e[33m"
-# N="\e[34m"
-
-# echo "Please enter the password:"
-# read -s mysql_root_password
-
-# VALIDATE(){
-#     if [ $1 -ne 0 ]
-#     then
-#         echo -e "$2...$R FAILURE $N"
-#         exit 1
-#     else
-#         echo -e "$2...$G SUCCESS $N"
-#     fi
-# }
-
-# if [ $USERID -ne 0 ]
-# then
-#     echo "Please run this script with root access."
-#     exit 1
-# else
-#     echo "your are a super user."
-# fi
-
-# dnf install nginx -y &>>$LOGFILE
-# VALIDATE $? "Installing nginx"
-
-# systemctl enable nginx &>>$LOGFILE
-# VALIDATE $? "Enabling nginx"
-
-# systemctl start nginx &>>$LOGFILE
-# VALIDATE $? "Starting nginx"
-
-# rm -rf /usr/share/nginx/html/* &>>$LOGFILE
-# VALIDATE $? "Removing existing content"
-
-# curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOGFILE
-# VALIDATE $? "Downloading frontend code"
-
-# cd /usr/share/nginx/html &>>$LOGFILE
-# unzip /tmp/frontend.zip &>>$LOGFILE
-# VALIDATE $? "Extracting the frontend code"
-
-# # Check your repo and path
-# cp /home/maintuser/shell-expense/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
-# VALIDATE $? "Copied nginx path"
-
-# systemctl restart ngnix &>>$LOGFILE
-# VALIDATE $? "Restart nginx service"
-
-
 #!/bin/bash
 
-source ./common.sh
-check_root
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[34m"
+
+echo "Please enter the password:"
+read -s mysql_root_password
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2...$R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2...$G SUCCESS $N"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1
+else
+    echo "your are a super user."
+fi
 
 dnf install nginx -y &>>$LOGFILE
 VALIDATE $? "Installing nginx"
@@ -79,11 +47,11 @@ VALIDATE $? "Downloading frontend code"
 
 cd /usr/share/nginx/html &>>$LOGFILE
 unzip /tmp/frontend.zip &>>$LOGFILE
-VALIDATE $? "Extracting frontend code"
+VALIDATE $? "Extracting the frontend code"
 
-#check your repo and path
+# Check your repo and path
 cp /home/maintuser/shell-expense/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
-VALIDATE $? "Copied expense conf"
+VALIDATE $? "Copied nginx path"
 
-systemctl restart nginx &>>$LOGFILE
-VALIDATE $? "Restarting nginx"
+systemctl restart ngnix &>>$LOGFILE
+VALIDATE $? "Restart nginx service"
--- mysql.sh
+++ mysql.sh
@@ -1,69 +1,45 @@
-# #!/bin/bash
-
-# USERID=$(id -u)
-# TIMESTAMP=$(date +%F-%H-%M-%S)
-# SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-# LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-# R="\e[31m"
-# G="\e[32m"
-# Y="\e[33m"
-# N="\e[34m"
-# echo "Please enter DB password:"
-# read -s mysql_root_password
-
-# VALIDATE(){
-#     if [ $1 -ne 0 ] # we can pass the orguments from outside $1 / -ne is the expression 
-#     then
-#         echo -e "$2...$R FAILURE $N"
-#         exit 1
-#     else
-#         echo -e "$2...$G SUCCESS $N"
-# fi
-
-# }
-
-# if [ $USERID -ne 0 ] #"$0 is contains script name"
-# then
-#     echo "Please run the script with root access."
-#     exit 1 #manually exit if error comes / other than 0 we can use any number
-# else
-#     echo "you are super user."
-# fi
-
-# dnf install mysql-server -y &>>$LOGFILE 
-# VALIDATE $? "Installating Mysql Server"
-
-# systemctl enable mysqld &>>$LOGFILE
-# VALIDATE $? "Enabling Mysql Server"
-
-# systemctl start mysqld &>>$LOGFILE
-# VALIDATE $? "starting Mysql Server"
-
-# # mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
-# # VALIDATE $? "Setting up root password"
-
-# # Below code will be useful for idempotency nature
-
 #!/bin/bash
 
-source ./common.sh
-
-check_root
-
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[34m"
 echo "Please enter DB password:"
 read -s mysql_root_password
 
-dnf install mysql-server -y &>>$LOGFILE
+VALIDATE(){
+    if [ $1 -ne 0 ] # we can pass the orguments from outside $1 / -ne is the expression 
+    then
+        echo -e "$2...$R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2...$G SUCCESS $N"
+fi
+
+}
+
+if [ $USERID -ne 0 ] #"$0 is contains script name"
+then
+    echo "Please run the script with root access."
+    exit 1 #manually exit if error comes / other than 0 we can use any number
+else
+    echo "you are super user."
+fi
+
+dnf install mysql-server -y &>>$LOGFILE 
+VALIDATE $? "Installating Mysql Server"
 
 systemctl enable mysqld &>>$LOGFILE
+VALIDATE $? "Enabling Mysql Server"
 
 systemctl start mysqld &>>$LOGFILE
+VALIDATE $? "starting Mysql Server"
 
-#Below code will be useful for idempotent nature
-#mysql -h 172.31.19.6 -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
-if [ $? -ne 0 ]
-then
-    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
-else
-    echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
-fi
+# mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+# VALIDATE $? "Setting up root password"
+
+# Below code will be useful for idempotency nature

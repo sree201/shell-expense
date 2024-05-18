ef06a62af9adb8150098648627de4318d9831290
Sree201 <sreesirinath4@gmail.com>
2024-05-18 12:23:41 -0700

    backend

--- mysql.sh
+++ mysql.sh
@@ -43,7 +43,7 @@ VALIDATE $? "Starting MySQL Server"
 #VALIDATE $? "Setting up root password"
 
 #Below code will be useful for idempotent nature
-mysql -h 172.31.19.64 -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+mysql -h 172.31.22.201 -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then
     mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE

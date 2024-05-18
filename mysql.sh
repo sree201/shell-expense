e9be0dda13972f1abc651bd18317d1ff7200bd35
Sree201 <sreesirinath4@gmail.com>
2024-05-18 12:13:21 -0700

    backend

--- mysql.sh
+++ mysql.sh
@@ -43,7 +43,7 @@ mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
 VALIDATE $? "Setting up root password"
 
 #Below code will be useful for idempotent nature
-mysql -h db.daws78s.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+mysql -h 172.31.19.64 -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then
     mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE

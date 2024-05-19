461515f82f2d395b624eb4bcec2ef51ea5ef7415
Sree201 <sreesirinath4@gmail.com>
2024-05-18 17:47:04 -0700

    frontend

--- mysql.sh
+++ mysql.sh
@@ -31,13 +31,13 @@ fi
 
 
 dnf install mysql-server -y &>>$LOGFILE
-VALIDATE $? "Installing MySQL Server"
+
 
 systemctl enable mysqld &>>$LOGFILE
-VALIDATE $? "Enabling MySQL Server"
+
 
 systemctl start mysqld &>>$LOGFILE
-VALIDATE $? "Starting MySQL Server"
+
 
 #mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
 #VALIDATE $? "Setting up root password"
@@ -47,7 +47,7 @@ mysql -h 172.31.22.201 -uroot -p${mysql_root_password} -e 'show databases;' &>>$
 if [ $? -ne 0 ]
 then
     mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
-    VALIDATE $? "MySQL Root password Setup"
+    
 else
     echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
 fi

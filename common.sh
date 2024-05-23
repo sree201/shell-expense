d92f472f66f93950c99285a4adb43a3b15747c22
Sree201 <sreesirinath4@gmail.com>
2024-05-22 17:21:17 -0700

    common

---
+++ common.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+echo "Please enter DB Password:"
+read -s mysql_root_password
